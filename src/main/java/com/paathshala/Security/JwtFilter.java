123b04060fa45ee5ef016ce481add8913af71272
Anish5703 <poudelanish11@gmail.com>
2025-12-04 22:12:29 +0545

    added security config and jwt filter

---
+++ src/main/java/com/paathshala/Security/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.paathshala.Security;
+
+import com.example.ByaparLink.Service.JwtService;
+import com.example.ByaparLink.Service.MyUserDetailsService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+@Autowired
+private JwtService jwtService;
+
+@Autowired
+private MyUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain filterChain) throws ServletException,
+            NullPointerException ,IOException, ExpiredJwtException, SignatureException {
+        String authHeader = req.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer "))
+        {
+            token = authHeader.substring(7);
+            username = jwtService.extractUsername(token);
+        }
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null)
+        {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if(jwtService.validateToken(token,userDetails))
+            {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource()
+                        .buildDetails(req));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+
+        }
+
+        filterChain.doFilter(req,resp);
+    }
+
+
+}
---
+++ src/main/java/com/paathshala/Security/SecurityConfig.java
@@ -0,0 +1,109 @@
+package com.paathshala.Security;
+
+
+import com.example.ByaparLink.Service.MyUserDetailsService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    private CustomOauth2SuccessHandler oauth2SuccessHandler;
+
+    @Autowired
+    private CustomOauth2FailureHandler oauth2FailureHandler;
+
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf(configurer -> configurer.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .oauth2Login(oauth2 ->
+                        oauth2
+                                .successHandler(oauth2SuccessHandler)
+                                .failureHandler(oauth2FailureHandler))
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(unauthorizedEntryPoint()) )
+                .sessionManagement(session->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+                .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
+
+                return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception
+    {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedEntryPoint()
+    {
+        return (request,response,authException) -> {
+            StringBuilder loginUrl = new StringBuilder();
+            loginUrl.append(request.getScheme()).append("://").append(request.getServerName()).append(":").append(request.getServerPort()).append("/api/auth/login");
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("{\"error\": \"Unauthorized - Please login at \""+loginUrl.toString());
+        };
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder(10);
+    }
+
+
+  public static final String[] PUBLIC_ENDPOINTS = {
+           "/api/auth/**",
+          "/api/home",
+          "/api/oauth/register",
+          "/oauth2/authorization/**",
+          "/login/oauth2/**",
+          "/api/oauth/**",
+          "/v3/api-docs/**",
+          "/swagger-ui/**",
+          "/swagger-ui.html"
+
+  };
+
+}
