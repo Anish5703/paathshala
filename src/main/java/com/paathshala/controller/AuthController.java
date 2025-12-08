b1d8ce37522cce6734f50214867e05fa58ac130c
Anish5703 <poudelanish11@gmail.com>
2025-12-08 09:47:45 +0545

    implemented authentication controller and open api dependency

---
+++ src/main/java/com/paathshala/controller/AuthController.java
@@ -0,0 +1,97 @@
+package com.paathshala.controller;
+
+
+import com.paathshala.DTO.Login.LoginRequest;
+import com.paathshala.DTO.Login.LoginResponse;
+import com.paathshala.DTO.Register.RegisterRequest;
+import com.paathshala.DTO.Register.RegisterResponse;
+import com.paathshala.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Operation(
+            summary = "Register User",
+            description = "Starts registration process using RegisterRequest DTO"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Check mail for confirmation link"),
+            @ApiResponse(responseCode = "400", description = "Registration Failed")
+    })
+    //Endpoint to register new user
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> registerUser(@Valid @RequestBody RegisterRequest req, HttpServletRequest servletRequest) {
+
+
+        RegisterResponse resp = authService.registerUser(req,servletRequest);
+        HttpHeaders header = new HttpHeaders();
+        header.set("Content-Type","application/json");
+        if (!resp.isError())
+            return ResponseEntity.status(HttpStatus.CREATED).headers(header).body(resp);
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(header).body(resp);
+        }
+
+      //Endpoint to confirm registration
+    @GetMapping("/confirmRegistration")
+    public ResponseEntity<RegisterResponse> confirmRegistration(@RequestParam(name="token")String token)
+    {
+        RegisterResponse resp = authService.validateRegisterConfirmation(token);
+        HttpHeaders header = new HttpHeaders();
+        header.set("Content-Type","application/json");
+        if(!resp.isError())
+            return ResponseEntity.status(HttpStatus.CREATED).headers(header).body(resp);
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(header).body(resp);
+    }
+    //Endpoint to send confirmation token
+    @PutMapping("/resendConfirmation")
+    public ResponseEntity<RegisterResponse> resendConfirmation(@RequestParam(name="email")String email,HttpServletRequest servletRequest)
+    {
+        RegisterResponse resp = authService.resendConfirmationToken(email,servletRequest);
+        HttpHeaders header = new HttpHeaders();
+        header.set("Content-Type","application/json");
+        if(!resp.isError())
+            return ResponseEntity.status(HttpStatus.OK).headers(header).body(resp);
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(header).body(resp);
+
+    }
+
+    //Endpoint to login user
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginRequest loginRequest)
+    {
+        LoginResponse resp = authService.loginUser(loginRequest);
+        HttpHeaders header = new HttpHeaders();
+        header.set("Content-Type","application/json");
+        if(!resp.isError()) {
+            header.set("Authorization","Bearer "+resp.getToken());
+            return ResponseEntity.status(HttpStatus.ACCEPTED).headers(header).body(resp);
+        }
+        else
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).headers(header).body(resp);
+    }
+
+
+
+    }
+
+
+
+
